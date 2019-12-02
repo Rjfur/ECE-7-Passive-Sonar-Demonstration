ba3e1e6d0b202d820602a3caf9798f47d557f4a5
jacobaubertine <jacobaubertine@gmail.com>
2019-12-01 23:48:05 -0500

    Added CDR test files, tested timing for 2 streams

---
+++ sounddevice_one_device_test.py
@@ -0,0 +1,30 @@
+import sounddevice as sd
+import numpy as np
+
+# trying to test using earbuds microphone
+# want to see contents of indata
+# are the two channels the same?
+
+def callback(indata, frames, time, status):
+    # Ldata = [indata[i][0] for i in range(len(indata))]
+    # Rdata = [indata[j][1] for j in range(len(indata))]
+    # print("####")
+    # print("L:")
+    # print(Ldata)
+    # print("R:")
+    # print(Rdata)
+    # print("Left channel matches right channel: ", np.array_equal(Ldata, Rdata))
+    for i in range(len(indata)):
+        Ldata = indata[i][0]
+        Rdata = indata[i][1]
+        if (Ldata != Rdata):
+            print("Left and right channels are not equal!")
+            print("Difference: ", Rdata - Ldata)
+    
+
+if __name__ == "__main__":
+    device_info = sd.query_devices()
+
+    stream = sd.InputStream(samplerate=44100, blocksize=0, device=0, channels=2, dtype=np.int16, latency="low", callback=callback)
+    stream.start()
+    stream.stop()
